f451ad6d69c5472742383738d4d41c4cbb4794ad
Brayan Emanuel Sanchez Ramirez <brayan.sanchez.contacto@gmail.com>
2023-08-27 13:32:45 -0600

    lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
+  {
+    path: '', //TODO: localhost:4200
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) //TODO: le estamos diciendo que en la ruta raiz va a cargar el home module de HomeModule
+  }
 ];
 
 @NgModule({
