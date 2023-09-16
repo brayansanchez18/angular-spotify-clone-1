e8519095462692696aec45413626c69e3db90c6c
Brayan Emanuel Sanchez Ramirez <brayan.sanchez.contacto@gmail.com>
2023-09-15 19:49:29 -0600

    formulario de login (formcontrol, formgoup y validators)

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', //TODO: http://localhost:4200/auth/login
+    component: LoginPageComponent,
+  },
+  {
+    //TODO: redireccionamos desde las rutas
+    path: '**',
+    redirectTo: 'auth/login',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
--- src/app/modules/auth/pages/login-page/login-page.component.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent implements OnInit {
+  formLogin: FormGroup = new FormGroup({});
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.formLogin = new FormGroup({
+      email: new FormControl('', [
+        Validators.required, // validamos que el campo de email no este vacio
+        Validators.email, // validamos que lo que este en el campo sea un correo valido
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(12),
+      ]),
+    });
   }
 
+  sendLogin(): void {}
 }
