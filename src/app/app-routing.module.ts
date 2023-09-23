3e25c71c5fb351d7f63b10c7b215c8f4d026d738
Brayan Emanuel Sanchez Ramirez <brayan.sanchez.contacto@gmail.com>
2023-09-22 22:29:03 -0600

    vigilantes de rutas (guards)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SessionGuard } from '@core/guards/session.guard';
 import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
 
 const routes: Routes = [
   {
     path: 'auth',
-    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then((m) => m.AuthModule),
   },
   {
     path: '', //TODO: localhost:4200
+    //TODO: private
     component: HomePageComponent,
-    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) //TODO: le estamos diciendo que en la ruta raiz va a cargar el home module de HomeModule
-  }
+    loadChildren: () =>
+      import('./modules/home/home.module').then((m) => m.HomeModule), //TODO: le estamos diciendo que en la ruta raiz va a cargar el home module de HomeModule
+    canActivate: [SessionGuard],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {
     //TODO: redireccionamos desde las rutas
     path: '**',
-    redirectTo: 'auth/login',
+    redirectTo: '/auth/login',
   },
 ];
 
