d698895b9910e587e80694bf6a49b1e563b0a04b
Brayan Emanuel Sanchez Ramirez <brayan.sanchez.contacto@gmail.com>
2023-09-23 20:34:10 -0600

    reproducir cancion al darle play en el card play component

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -8,27 +8,11 @@ import { MultimediaService } from '@shared/services/multimedia.service';
   styleUrls: ['./media-player.component.css'],
 })
 export class MediaPlayerComponent implements OnInit, OnDestroy {
-  mockCover: TrackModel = {
-    _id: 7,
-    name: 'Rayas de Patron',
-    album: 'Soy la Bomba',
-    cover:
-      'https://source.boomplaymusic.com/group10/M00/03/21/59a2173be2ef42b28ae72a5bd1d47883_320_320.jpg',
-    url: 'http://loclocalhost/track.mp3',
-  };
-
   listObservers$: Array<any> = [];
 
-  constructor(private multimediaservice: MultimediaService) {}
+  constructor(public multimediaservice: MultimediaService) {}
 
-  ngOnInit(): void {
-    const observer1$ = this.multimediaservice.callback.subscribe(
-      (response: TrackModel) => {
-        // console.log('resibiendo cancion: ' + response);
-      }
-    );
-    this.listObservers$ = [observer1$];
-  }
+  ngOnInit(): void {}
 
   ngOnDestroy(): void {
     this.listObservers$.forEach((u) => u.unsubscribe());
--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -1,9 +1,31 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+import { BehaviorSubject, Observable, Observer } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MultimediaService {
   callback: EventEmitter<any> = new EventEmitter<any>();
-  constructor() {}
+  public trackInfo$: BehaviorSubject<any> = new BehaviorSubject(undefined);
+  public audio!: HTMLAudioElement;
+
+  constructor() {
+    this.audio = new Audio();
+    this.trackInfo$.subscribe((responseOk) => {
+      if (responseOk) {
+        this.setAudio(responseOk);
+      }
+    });
+  }
+
+  private listenAllEvents(): void {}
+
+  //TODO: funciones publicas
+  public setAudio(track: TrackModel): void {
+    console.log('ok', track);
+    this.audio.src = track.url;
+    console.log(this.audio.src);
+    this.audio.play();
+  }
 }
